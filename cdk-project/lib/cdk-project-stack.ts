6f589c239f6467015796eb71388817b95fdeed21
aaliaksandrau <a.aliaksandrau@elinext.com>
2024-06-14 16:50:21 +0400

    feat: add cdk autodeployment

--- cdk-project/lib/cdk-project-stack.ts
+++ cdk-project/lib/cdk-project-stack.ts
@@ -1,16 +1,67 @@
 import * as cdk from 'aws-cdk-lib';
+import {
+  aws_cloudfront as cloudfront,
+  aws_s3 as s3,
+  aws_s3_deployment as s3deploy,
+  aws_iam as iam,
+} from "aws-cdk-lib";
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const cloudfrontOAI = new cloudfront.OriginAccessIdentity(
+      this,
+      "cloudfront-OAI",
+      {
+        comment: 'OriginAccessIdentity for my-first-shop-app'
+      }
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkProjectQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const myStoreBucket = new s3.Bucket(this, "myStoreBucket", {
+      bucketName: "my-first-shop-app-cdk",
+      websiteIndexDocument: "index.html",
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      versioned: false,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    myStoreBucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        actions: ["s3:GetObject"],
+        resources: [myStoreBucket.arnForObjects("*")],
+        principals: [
+          new iam.CanonicalUserPrincipal(
+            cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId
+          ),
+        ],
+      })
+    );
+
+    const distribution = new cloudfront.CloudFrontWebDistribution(
+      this,
+      "MyCloudfrontDistribution",
+      {
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: myStoreBucket,
+              originAccessIdentity: cloudfrontOAI,
+            },
+            behaviors: [{ isDefaultBehavior: true }],
+          },
+        ],
+      }
+    );
+
+    new s3deploy.BucketDeployment(this, "MyBucketDeployment", {
+      sources: [s3deploy.Source.asset("../dist")],
+      destinationBucket: myStoreBucket,
+      distribution,
+      distributionPaths: ["/*"],
+    });
   }
 }
