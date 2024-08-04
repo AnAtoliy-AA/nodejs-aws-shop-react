2d92dc5bad75fa5d5542cca9bcd73377b0bd934f
aaliaksandrau <a.aliaksandrau@elinext.com>
2024-08-04 18:49:08 +0400

    fix: bff endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://57tj5heb4i.execute-api.eu-central-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://cql427w9ce.execute-api.eu-central-1.amazonaws.com/prod",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "http://anatoliy-aa-bff-api-dev.us-west-2.elasticbeanstalk.com/",
   cart: "http://anatoliy-aa-cart-api-dev.eu-central-1.elasticbeanstalk.com",
 };
 
