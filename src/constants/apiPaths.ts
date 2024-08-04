facfb4cb41a4d1efd5b915aa9d8be18ed1d0de09
aaliaksandrau <a.aliaksandrau@elinext.com>
2024-08-04 18:36:48 +0400

    fix: cart endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://57tj5heb4i.execute-api.eu-central-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://cql427w9ce.execute-api.eu-central-1.amazonaws.com/prod/",
+  import: "https://cql427w9ce.execute-api.eu-central-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "http://anatoliy-aa-cart-api-dev.eu-central-1.elasticbeanstalk.com",
 };
 
 export default API_PATHS;
